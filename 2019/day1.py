bf785cb786df98a7407426735e12142bca947745
am-berry <46605511+am-berry@users.noreply.github.com>
2019-12-01 19:41:13 +0000

    refactored code

--- 2019/day1.py
+++ 2019/day1.py
@@ -1,29 +1,25 @@
 #/usr/bin/env python
 
 def mass_calculator(x):
-  calc = (x // 3)
+  calc = x // 3
   calc -= 2
-  return int(calc)
-
-with open('input.txt') as fp:
-  fuel_list = 0
-  for line in fp:
-    fuel_list += mass_calculator(int(line))
-fp.close()
-print(fuel_list)
+  return calc
 
 def mass_calculator_recursive(x):
   fuel = mass_calculator(x)
   temp = fuel
   while temp > 5:
-    fuel += mass_calculator(int(temp))
-    temp = mass_calculator(int(temp))
+    fuel += mass_calculator(temp)
+    temp = mass_calculator(temp)
   return fuel
 
-with open('input.txt') as fp:
+with open('day1_input.txt') as fp:
   fuel_cnt = 0
+  recursive_fuel_cnt = 0
   for line in fp:
-    fuel_cnt += mass_calculator_recursive(int(line))
+    fuel_cnt += mass_calculator(int(line))
+    recursive_fuel_cnt += mass_calculator_recursive(int(line))
 fp.close()
-print(fuel_cnt)
 
+print(fuel_cnt)
+print(recursive_fuel_cnt)
